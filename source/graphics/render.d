19490bbba7c86971a04c767dc0f701cc5bec8271
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-04-05 04:07:25 -0400

    Good

--- source/graphics/render.d
+++ source/graphics/render.d
@@ -12,7 +12,7 @@ private:
 public: //* BEGIN PUBLIC API.
 
     void rectangle(Vec2d position, Vec2d size, Color color) {
-        DrawRectangleV(invertPosition(position).toRaylib(), size.toRaylib(), color);
+        DrawRectangleV(position.toRaylib(), size.toRaylib(), color);
     }
 
     void rectangleLines(Vec2d position, Vec2d size, Color color, double thickness = 0.01) {
