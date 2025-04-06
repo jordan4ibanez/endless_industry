35ef7d05a5018faf3f1ef5c76aa47ebf0aef4cb1
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-04-06 08:15:28 -0400

    This needs a rework

--- source/utility/collision_functions.d
+++ source/utility/collision_functions.d
@@ -20,73 +20,73 @@ struct CollisionResult {
 //? Note: This will have issues extremely far out.
 private immutable double magicAdjustment = 0.0001;
 
-CollisionResult collideXToTile(Vec2d entityPosition, Vec2d entitySize, Vec2d entityVelocity,
-    Vec2d tilePosition, Vec2d tileSize) {
+// CollisionResult collideXToTile(Vec2d entityPosition, Vec2d entitySize, Vec2d entityVelocity,
+//     Vec2d tilePosition, Vec2d tileSize) {
 
-    CollisionResult result;
-    result.newPosition = entityPosition.x;
+//     CollisionResult result;
+//     result.newPosition = entityPosition.x;
 
-    int dir = cast(int) sgn(entityVelocity.x);
+//     int dir = cast(int) sgn(entityVelocity.x);
 
-    // This thing isn't moving.
-    if (dir == 0) {
-        return result;
-    }
+//     // This thing isn't moving.
+//     if (dir == 0) {
+//         return result;
+//     }
 
-    // Entity position is on the bottom center of the collisionbox.
-    const double entityHalfWidth = entitySize.x * 0.5;
-    const Rect entityRectangle = Rect(entityPosition.x - entityHalfWidth, entityPosition.y,
-        entitySize.x, entitySize.y);
+//     // Entity position is on the bottom center of the collisionbox.
+//     const double entityHalfWidth = entitySize.x * 0.5;
+//     const Rect entityRectangle = Rect(entityPosition.x - entityHalfWidth, entityPosition.y,
+//         entitySize.x, entitySize.y);
 
-    const Rect tileRectangle = Rect(tilePosition.x, tilePosition.y, tileSize.x, tileSize.y);
+//     const Rect tileRectangle = Rect(tilePosition.x, tilePosition.y, tileSize.x, tileSize.y);
 
-    if (checkCollisionRecs(entityRectangle, tileRectangle)) {
-        // This doesn't kick out in a specific direction on dir 0 because the Y axis check will kick them up as a safety.
-        result.collides = true;
-        if (dir > 0) {
-            // Kick left.
-            result.newPosition = tilePosition.x - entityHalfWidth - magicAdjustment;
-        } else if (dir < 0) {
-            // Kick right.
-            result.newPosition = tilePosition.x + tileSize.x + entityHalfWidth + magicAdjustment;
-        }
-    }
+//     if (checkCollisionRecs(entityRectangle, tileRectangle)) {
+//         // This doesn't kick out in a specific direction on dir 0 because the Y axis check will kick them up as a safety.
+//         result.collides = true;
+//         if (dir > 0) {
+//             // Kick left.
+//             result.newPosition = tilePosition.x - entityHalfWidth - magicAdjustment;
+//         } else if (dir < 0) {
+//             // Kick right.
+//             result.newPosition = tilePosition.x + tileSize.x + entityHalfWidth + magicAdjustment;
+//         }
+//     }
 
-    return result;
-}
+//     return result;
+// }
 
-CollisionResult collideYToTile(Vec2d entityPosition, Vec2d entitySize, Vec2d entityVelocity,
-    Vec2d tilePosition, Vec2d tileSize) {
+// CollisionResult collideYToTile(Vec2d entityPosition, Vec2d entitySize, Vec2d entityVelocity,
+//     Vec2d tilePosition, Vec2d tileSize) {
 
-    CollisionResult result;
-    result.newPosition = entityPosition.y;
+//     CollisionResult result;
+//     result.newPosition = entityPosition.y;
 
-    int dir = cast(int) sgn(entityVelocity.y);
+//     int dir = cast(int) sgn(entityVelocity.y);
 
-    // This thing isn't moving.
-    if (dir == 0) {
-        return result;
-    }
+//     // This thing isn't moving.
+//     if (dir == 0) {
+//         return result;
+//     }
 
-    // Entity position is on the bottom center of the collisionbox.
-    const double entityHalfWidth = entitySize.x * 0.5;
-    const Rect entityRectangle = Rect(entityPosition.x - entityHalfWidth, entityPosition.y,
-        entitySize.x, entitySize.y);
+//     // Entity position is on the bottom center of the collisionbox.
+//     const double entityHalfWidth = entitySize.x * 0.5;
+//     const Rect entityRectangle = Rect(entityPosition.x - entityHalfWidth, entityPosition.y,
+//         entitySize.x, entitySize.y);
 
-    const Rect tileRectangle = Rect(tilePosition.x, tilePosition.y, tileSize.x, tileSize.y);
+//     const Rect tileRectangle = Rect(tilePosition.x, tilePosition.y, tileSize.x, tileSize.y);
 
-    if (checkCollisionRecs(entityRectangle, tileRectangle)) {
+//     if (checkCollisionRecs(entityRectangle, tileRectangle)) {
 
-        result.collides = true;
-        if (dir <= 0) {
-            // Kick up. This is the safety default.
-            result.newPosition = tilePosition.y + tileSize.y + magicAdjustment;
-            result.hitGround = true;
-        } else {
-            // Kick down.
-            result.newPosition = tilePosition.y - entitySize.y - magicAdjustment;
-        }
-    }
+//         result.collides = true;
+//         if (dir <= 0) {
+//             // Kick up. This is the safety default.
+//             result.newPosition = tilePosition.y + tileSize.y + magicAdjustment;
+//             result.hitGround = true;
+//         } else {
+//             // Kick down.
+//             result.newPosition = tilePosition.y - entitySize.y - magicAdjustment;
+//         }
+//     }
 
-    return result;
-}
+//     return result;
+// }
