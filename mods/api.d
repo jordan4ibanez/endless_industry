407f3c919e77c00ff1a6afd07797bff1f6725e92
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-04-06 09:10:29 -0400

    Explain the mods preprocessor

--- mods/api.d
+++ mods/api.d
@@ -4,6 +4,8 @@ import game.biome_database;
 import game.tile_database;
 
 // Do not modify the autos. :)
+// These are part of a preprocessor I made to auto bake your mod into the game itself.
+// I also probably wouldn't duplicate these comments unless you like seeing the file disappear lol.
 
 //# =-AUTO IMPORT BEGIN-=
 import mods.cube_thing.main;
