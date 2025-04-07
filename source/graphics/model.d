3f97142eae3dc175a0cef688e4f3cad83f957e81
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-04-07 13:08:53 -0400

    Roll a custom function to get consistent 0 time

--- source/graphics/model.d
+++ source/graphics/model.d
@@ -7,6 +7,34 @@ import std.conv;
 import std.stdio;
 import std.string;
 
+void fastMatrixMultiply(const Matrix* left, const Matrix* right, Matrix* result) {
+
+    result.m0 = left.m0 * right.m0 + left.m1 * right.m4 + left.m2 * right.m8 + left.m3 * right.m12;
+    result.m1 = left.m0 * right.m1 + left.m1 * right.m5 + left.m2 * right.m9 + left.m3 * right.m13;
+    result.m2 = left.m0 * right.m2 + left.m1 * right.m6 + left.m2 * right.m10 + left.m3 * right.m14;
+    result.m3 = left.m0 * right.m3 + left.m1 * right.m7 + left.m2 * right.m11 + left.m3 * right.m15;
+    result.m4 = left.m4 * right.m0 + left.m5 * right.m4 + left.m6 * right.m8 + left.m7 * right.m12;
+    result.m5 = left.m4 * right.m1 + left.m5 * right.m5 + left.m6 * right.m9 + left.m7 * right.m13;
+    result.m6 = left.m4 * right.m2 + left.m5 * right.m6 + left.m6 * right.m10 + left.m7 * right.m14;
+    result.m7 = left.m4 * right.m3 + left.m5 * right.m7 + left.m6 * right.m11 + left.m7 * right.m15;
+    result.m8 = left.m8 * right.m0 + left.m9 * right.m4 + left.m10 * right.m8 + left.m11 * right
+        .m12;
+    result.m9 = left.m8 * right.m1 + left.m9 * right.m5 + left.m10 * right.m9 + left.m11 * right
+        .m13;
+    result.m10 = left.m8 * right.m2 + left.m9 * right.m6 + left.m10 * right.m10 + left.m11 * right
+        .m14;
+    result.m11 = left.m8 * right.m3 + left.m9 * right.m7 + left.m10 * right.m11 + left.m11 * right
+        .m15;
+    result.m12 = left.m12 * right.m0 + left.m13 * right.m4 + left.m14 * right.m8 + left.m15 * right
+        .m12;
+    result.m13 = left.m12 * right.m1 + left.m13 * right.m5 + left.m14 * right.m9 + left.m15 * right
+        .m13;
+    result.m14 = left.m12 * right.m2 + left.m13 * right.m6 + left.m14 * right.m10 + left.m15 * right
+        .m14;
+    result.m15 = left.m12 * right.m3 + left.m13 * right.m7 + left.m14 * right.m11 + left.m15 * right
+        .m15;
+}
+
 static final const class ModelHandler {
 static:
 private:
@@ -123,10 +151,10 @@ public: //* BEGIN PUBLIC API.
         Matrix matProjection = rlGetMatrixProjection();
         Matrix matrixTransform = rlGetMatrixTransform();
 
-        matModel = MatrixMultiply(transform, matrixTransform);
+        fastMatrixMultiply(&transform, &matrixTransform, &matModel);
 
         // Get model-view matrix
-        matModelView = MatrixMultiply(matModel, matView);
+        fastMatrixMultiply(&matModel, &matView, &matModelView);
 
         // Calculate model-view-projection matrix (MVP)
         Matrix matModelViewProjection;
@@ -134,7 +162,7 @@ public: //* BEGIN PUBLIC API.
         matModelViewProjection.m5 = 1;
         matModelViewProjection.m10 = 1;
         matModelViewProjection.m15 = 1;
-        matModelViewProjection = MatrixMultiply(matModelView, matProjection);
+        fastMatrixMultiply(&matModelView, &matProjection, &matModelViewProjection);
 
         // Send combined model-view-projection matrix to shader
         rlSetUniformMatrix(mvpUniformLocation, matModelViewProjection);
