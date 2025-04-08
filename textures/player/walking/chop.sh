045336e911c7ed65ab5ae1fe8a260a5093b93032
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-04-08 15:57:11 -0400

    Time for walking

---
+++ textures/player/walking/chop.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+HORIZONTAL_FRAMES=$((4 - 1))
+DIRECTIONS=$((8 - 1))
+FRAME_SIZE=128
+SOURCE_FILE="character_standing_cycles_raw.png"
+OUTPUT_FRAME_SIZE=$((FRAME_SIZE - 40))
+FRAME_SHIFT=20
+
+
+for Y in $(seq 0 $DIRECTIONS); do
+
+    OFFSET_Y=$(((FRAME_SIZE * $Y) + FRAME_SHIFT))
+
+    OUTPUT_FOLDER=./direction_${Y}
+
+    mkdir -p ${OUTPUT_FOLDER}
+
+    for X in $(seq 0 $HORIZONTAL_FRAMES); do
+
+        OFFSET_X=$(((FRAME_SIZE * $X) + FRAME_SHIFT))
+
+        OUTPUT_NAME="player_standing_direction_${Y}_frame_${X}"
+
+        # echo "$OUTPUT_NAME"
+        # echo "$OFFSET_X | $OFFSET_Y"
+
+        convert -extract ${OUTPUT_FRAME_SIZE}x${OUTPUT_FRAME_SIZE}+${OFFSET_X}+${OFFSET_Y} ${SOURCE_FILE} ${OUTPUT_FOLDER}/${OUTPUT_NAME}.png
+
+    done
+done
