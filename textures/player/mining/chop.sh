61dfe92e502be3196ee6d19ea2c36e75c972f145
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-04-08 15:25:20 -0400

    Checkpoint in case I blow this thing up

--- textures/player/mining/chop.sh
+++ textures/player/mining/chop.sh
@@ -1,3 +1,33 @@
 #!/bin/bash
 
-convert -extract 128x128+0+0 character_mining_cycles_raw.png 1.png
+# convert -extract 128x128+0+0 character_mining_cycles_raw.png 1.png
+
+
+HORIZONTAL_FRAMES=$((4 - 1))
+DIRECTIONS=$((8 - 1))
+FRAME_SIZE=128
+
+for X in $(seq 0 $HORIZONTAL_FRAMES); do
+
+    OFFSET_X=$((FRAME_SIZE * $X))
+
+    for Y in $(seq 0 $DIRECTIONS); do
+
+        OFFSET_Y=$((FRAME_SIZE * $Y))
+
+        OUTPUT_NAME="player_mining_direction_${Y}_frame_${X}"
+
+        echo "$OUTPUT_NAME"
+
+
+        # convert -extract 128x128+0+0 character_mining_cycles_raw.png 1.png
+
+
+
+        
+        
+
+        echo "$OFFSET_X | $OFFSET_Y"
+
+    done
+done
