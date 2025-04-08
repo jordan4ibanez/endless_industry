cc158fd96974574df1770147c11ab31a08c3fefc
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-04-08 15:52:24 -0400

    Squish the frame

--- textures/player/mining/chop.sh
+++ textures/player/mining/chop.sh
@@ -6,10 +6,13 @@ HORIZONTAL_FRAMES=$((4 - 1))
 DIRECTIONS=$((8 - 1))
 FRAME_SIZE=128
 SOURCE_FILE="character_mining_cycles_raw.png"
+OUTPUT_FRAME_SIZE=$((FRAME_SIZE - 40))
+FRAME_SHIFT=20
+
 
 for Y in $(seq 0 $DIRECTIONS); do
 
-    OFFSET_Y=$((FRAME_SIZE * $Y))
+    OFFSET_Y=$(((FRAME_SIZE * $Y) + FRAME_SHIFT))
 
     OUTPUT_FOLDER=./direction_${Y}
 
@@ -17,14 +20,14 @@ for Y in $(seq 0 $DIRECTIONS); do
 
     for X in $(seq 0 $HORIZONTAL_FRAMES); do
 
-        OFFSET_X=$((FRAME_SIZE * $X))
+        OFFSET_X=$(((FRAME_SIZE * $X) + FRAME_SHIFT))
 
         OUTPUT_NAME="player_mining_direction_${Y}_frame_${X}"
 
         # echo "$OUTPUT_NAME"
         # echo "$OFFSET_X | $OFFSET_Y"
 
-        convert -extract ${FRAME_SIZE}x${FRAME_SIZE}+${OFFSET_X}+${OFFSET_Y} ${SOURCE_FILE} ${OUTPUT_FOLDER}/${OUTPUT_NAME}.png
+        convert -extract ${OUTPUT_FRAME_SIZE}x${OUTPUT_FRAME_SIZE}+${OFFSET_X}+${OFFSET_Y} ${SOURCE_FILE} ${OUTPUT_FOLDER}/${OUTPUT_NAME}.png
 
     done
 done
