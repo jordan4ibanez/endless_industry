1a79e71ca6b4dc05bc1ed7a0d464608322cc27a0
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-04-09 16:18:03 -0400

    Create ore_database.d

---
+++ source/game/ore_database.d
@@ -0,0 +1,15 @@
+module game.ore_database;
+
+struct OreDefinition {
+    string name = null;
+
+    ///! DO NOT USE.
+}
+
+static final const class BiomeDatabase {
+static:
+private:
+
+    string modName = null;
+
+}
