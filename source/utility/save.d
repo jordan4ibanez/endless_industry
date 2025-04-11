24dc9300dfdd5423d4434c1a93023dbd6e030fa4
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-04-11 14:14:13 -0400

    Put some training wheels on

--- source/utility/save.d
+++ source/utility/save.d
@@ -1,19 +1,21 @@
 module utility.save;
 
 import d2sqlite3;
+import optibrev;
 
 static final const class Save {
 static:
 private:
 
-    Database database;
+    Option!Database database;
 
 public: //* BEGIN PUBLIC API.
 
     void open(string saveName) {
         const string location = "saves/";
         const string fileExtension = ".sqlite";
-        database = Database(location ~ saveName ~ fileExtension);
+        database = database.Some(Database(location ~ saveName ~ fileExtension));
+
     }
 
 private: //* BEGIN INTERNAL API.
