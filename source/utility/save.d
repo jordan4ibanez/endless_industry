d28a9bf5215a325dfa63190367b1ddff19d87d7e
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-04-11 13:52:56 -0400

    Rename

---
+++ source/utility/save.d
@@ -0,0 +1,18 @@
+module utility.save;
+
+import d2sqlite3;
+
+static final const class Save {
+static:
+private:
+
+public: //* BEGIN PUBLIC API.
+
+    void openSave() {
+        Database db = Database("test.sqlite");
+
+    }
+
+private: //* BEGIN INTERNAL API.
+
+}
