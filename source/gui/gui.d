a55ecc6a688a8eacac0e7fcef4ae9efb13690f35
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-04-12 17:19:48 -0400

    I really like where this is going :D

--- source/gui/gui.d
+++ source/gui/gui.d
@@ -1,4 +1,9 @@
 module gui.gui;
+
+import graphics.colors;
+import math.vec2d;
+import math.vec2i;
+
 /*
 *
 *   CONTROLS PROVIDED:
@@ -43,15 +48,44 @@ class Element {
 class Container {
     bool visible = true;
 
+    // Position is top left of container.
+    Vec2i position;
+    Vec2i size;
+
+    // The infill color of the window.
+    Color infillColor = Colors.GRAY;
+    // The border color of the window. (All border components.)
+    Color borderColor = Colors.BLACK;
+
 }
 
 static final const class Gui {
 static:
 private:
 
+    // We standardize the GUI with 1080p.
+    const Vec2d standardSize = Vec2d(1920.0, 1080.0);
+    double currentGUIScale = 1.0;
+
     Container[string] interfaces;
 
 public: //* BEGIN PUBLIC API.
+
+    double getGUIScale() {
+        return currentGUIScale;
+    }
+
+    void __update(Vec2d newWindowSize) {
+        // Find out which GUI scale is smaller so things can be scaled around it.
+
+        Vec2d scales = Vec2d(newWindowSize.x / standardSize.x, newWindowSize.y / standardSize.y);
+
+        if (scales.x >= scales.y) {
+            currentGUIScale = scales.y;
+        } else {
+            currentGUIScale = scales.x;
+        }
+    }
 
 private: //* BEGIN INTERNAL API.
 
