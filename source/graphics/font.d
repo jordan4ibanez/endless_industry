7f0b80fe322ab98f48e61641a2252018e7970d50
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-04-12 17:28:42 -0400

    Fix up font

--- source/graphics/font.d
+++ source/graphics/font.d
@@ -1,47 +1,45 @@
 module graphics.font;
 
+import gui.gui;
 import raylib;
 import std.math.rounding;
 import std.string;
-import gui.gui;
 
 static final const class FontHandler {
 static:
 private:
 
     // Roboto condensed medium looks pretty close to the Bass Rise font, kind of.
-    Font* font = null;
+    Font font;
     const double spacing = -1;
     double currentFontSize = 1;
 
 public: //* BEGIN PUBLIC API.
 
     void initialize() {
-        font = new Font();
 
         dstring codePointString = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_-+={[]}|" ~
             "\\;:'\",<.>©";
 
-        *font = LoadFontEx(
+        font = LoadFontEx(
             toStringz("font/roboto_condensed.ttf"), 64, cast(int*) codePointString, 0);
     }
 
     Vector2 getTextSize(string text) {
-        return MeasureTextEx(*font, toStringz(text), currentFontSize, spacing);
+        return MeasureTextEx(font, toStringz(text), currentFontSize, spacing);
     }
 
     void draw(string text, double x, double y, Color color = Colors.BLACK) {
-        DrawTextEx(*font, toStringz(text), Vector2(x, y), currentFontSize, spacing, color);
+        DrawTextEx(font, toStringz(text), Vector2(x, y), currentFontSize, spacing, color);
     }
 
     void drawShadowed(string text, double x, double y, Color foregroundColor = Colors.WHITE) {
-        DrawTextEx(*font, toStringz(text), Vector2(x, y), currentFontSize, spacing, Colors.BLACK);
-        DrawTextEx(*font, toStringz(text), Vector2(x - 1, y - 1), currentFontSize, spacing, foregroundColor);
+        DrawTextEx(font, toStringz(text), Vector2(x, y), currentFontSize, spacing, Colors.BLACK);
+        DrawTextEx(font, toStringz(text), Vector2(x - 1, y - 1), currentFontSize, spacing, foregroundColor);
     }
 
     void terminate() {
-        UnloadFont(*font);
-        font = null;
+        UnloadFont(font);
     }
 
     void __update() {
