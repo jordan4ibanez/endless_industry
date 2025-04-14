542037ca7ab7dae5cc67a928884326eacfa63e08
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-04-14 16:33:54 -0400

    Oops, make the documentation work

--- source/gui/window_gui.d
+++ source/gui/window_gui.d
@@ -11,73 +11,73 @@ import std.math.rounding;
 class WindowGUI {
 package:
 
-    // The ID of this window.
+    /// The ID of this window.
     string windowID = null;
 
-    // These are hidden because they're kept in rendering order.
+    /// These are hidden because they're kept in rendering order.
     Component[] componentsInOrder;
     Component[string] componentDatabase;
 
 public:
 
-    // If it's a window, this defines if you can resize it.
+    /// If it's a window, this defines if you can resize it.
     bool resizeable = true;
 
-    // What this window's title says.
+    /// What this window's title says.
     string windowTitle = null;
 
     //? State behavior.
 
-    // Position is top left of window.
+    /// Position is top left of window.
     Vec2i position;
 
-    // The current size of the window. (It will always be scaled to GUI scaling)
+    /// The current size of the window. (It will always be scaled to GUI scaling)
     Vec2i size;
 
-    // The minimum size of the window. (It will always be scaled to GUI scaling)
+    /// The minimum size of the window. (It will always be scaled to GUI scaling)
     Vec2i minSize = Vec2i(100, 100);
 
-    // If the mouse is hovering over the status bar.
+    /// If the mouse is hovering over the status bar.
     bool mouseHoveringStatusBar = false;
 
-    // If the mouse is hovering over the close button.
+    /// If the mouse is hovering over the close button.
     bool mouseHoveringCloseButton = false;
 
-    // If the mouse is hovering over the resize button.
+    /// If the mouse is hovering over the resize button.
     bool mouseHoveringResizeButton = false;
 
     //? General solid colors.
 
-    // The color of the work area.
+    /// The color of the work area.
     Color workAreaColor = Colors.GRAY;
 
-    // The border color of the window. (All border components.)
+    /// The border color of the window. (All border components.)
     Color borderColor = Colors.BLACK;
 
-    // The status bar background color.
+    /// The status bar background color.
     Color statusBarColor = Colors.BLUE;
-    // The status bar background color when hovered over.
+    /// The status bar background color when hovered over.
     Color statusBarHoverColor = Color(40, 50, 255, 255);
 
-    // The close button background color.
+    /// The close button background color.
     Color closeButtonBackgroundColor = Colors.GRAY;
 
-    // The resize button background color.
+    /// The resize button background color.
     Color resizeButtonBackgroundColor = Colors.GRAY;
-    // The resize button background color when hovered over.
+    /// The resize button background color when hovered over.
     Color resizeButtonBackgroundColorHovered = Colors.DARKGRAY;
 
     //? General text/icon colors.
 
-    // The text below the status bar.
+    /// The text below the status bar.
     Color workAreaTextColor = Colors.BLACK;
 
-    // The status bar text color.
+    /// The status bar text color.
     Color statusBarTextColor = Colors.WHITE;
 
-    // The close button X color.
+    /// The close button X color.
     Color closeButtonXColor = Colors.BLACK;
-    // The close button X color when hovered over.
+    /// The close button X color when hovered over.
     Color closeButtonXHoverColor = Colors.RED;
 
     /// Center the window.
@@ -92,6 +92,7 @@ public:
         position.y = newPositionY;
     }
 
+    /// Add a component into the window's work area.
     void addComponent(string componentID, Component component) {
         component.componentID = componentID;
 
@@ -100,6 +101,7 @@ public:
         componentDatabase[componentID] = component;
     }
 
+    /// Get a component of the window's work area.
     Option!Component getComponent(string componentID) {
         Option!Component result;
         Component* thisComponent = componentID in componentDatabase;
