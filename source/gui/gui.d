cef35475280ae9cc1226a0289783a9b787fd6c61
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-04-14 18:23:12 -0400

    Squish, doc, implement

--- source/gui/gui.d
+++ source/gui/gui.d
@@ -253,43 +253,35 @@ public: //* BEGIN PUBLIC API.
         EndScissorMode();
     }
 
+    /// This draws the resize button on the bottom right of the window.
     void drawResizeButton() {
-
         //? Draw the resize button.
-
         const int posX = cast(int) floor(centerPoint.x + (currentWindow.position.x * currentGUIScale));
         const int posY = cast(int) floor(centerPoint.y + (currentWindow.position.y * currentGUIScale));
         const int sizeX = cast(int) floor(currentWindow.size.x * currentGUIScale);
         const int sizeY = cast(int) floor(currentWindow.size.y * currentGUIScale);
         const int statusAreaHeight = cast(int) floor(currentGUIScale * 32.0);
-
         const int halfStatusAreaHeight = cast(int) floor(statusAreaHeight * 0.5);
-
         BeginScissorMode(
             posX + sizeX - halfStatusAreaHeight - 1,
             posY + sizeY - halfStatusAreaHeight - 1,
             halfStatusAreaHeight + 1,
             halfStatusAreaHeight + 1);
-
         const Color resizeButtonColor = currentWindow.mouseHoveringResizeButton ? currentWindow
             .resizeButtonBackgroundColorHovered : currentWindow.resizeButtonBackgroundColor;
-
         DrawRectangle(
             posX + sizeX - halfStatusAreaHeight,
             posY + sizeY - halfStatusAreaHeight,
             halfStatusAreaHeight,
             halfStatusAreaHeight,
             resizeButtonColor);
-
         DrawRectangleLines(
             posX + sizeX - halfStatusAreaHeight,
             posY + sizeY - halfStatusAreaHeight,
             halfStatusAreaHeight,
             halfStatusAreaHeight,
             currentWindow.borderColor);
-
         EndScissorMode();
-
     }
 
     void drawCurrentWindow() {
@@ -299,6 +291,10 @@ public: //* BEGIN PUBLIC API.
         }
 
         drawWindowFrame();
+
+        // todo: do the draw components function here.
+
+        drawResizeButton();
 
     }
 
