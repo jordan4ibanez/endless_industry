1d7e3745deb14d1eb8e7ac3d925cf4f2e09ec4b1
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-04-14 22:27:10 -0400

    Cobble together an audio thing

--- source/audio/audio.d
+++ source/audio/audio.d
@@ -1,6 +1,10 @@
 module audio.audio;
 
 import raylib;
+import std.file;
+import std.path;
+import std.stdio;
+import std.string;
 
 static final const class Audio {
 static:
@@ -12,17 +16,49 @@ public: //* BEGIN PUBLIC API.
 
     void initialize() {
         InitAudioDevice();
+        loadAllSounds();
     }
 
     void terminate() {
-        foreach (sound; database) {
+        foreach (name, sound; database) {
+            // writeln("unloaded sound: ", name);
             UnloadSound(sound);
         }
         CloseAudioDevice();
     }
 
+private: //* BEGIN INTERNAL API.
+
+    void loadAllSounds() {
+        foreach (string thisFilePathString; dirEntries("sounds", "*{.wav,.ogg}", SpanMode.depth)) {
+            loadSound(thisFilePathString);
+        }
+    }
+
     void loadSound(string location) {
 
+        // Extract the file name from the location.
+        string fileName = () {
+            string[] items = location.split("/");
+            int len = cast(int) items.length;
+            if (len <= 1) {
+                throw new Error("Sound must not be in root directory.");
+            }
+            string outputFileName = items[len - 1];
+            if (!outputFileName.endsWith(".wav") && !outputFileName.endsWith(".ogg")) {
+                throw new Error("Not .ogg or .wav");
+            }
+            return outputFileName;
+        }();
+
+        Sound thisSound = LoadSound(toStringz(location));
+
+        if (fileName in database) {
+            throw new Error("Duplicate sound: " ~ location);
+        }
+
+        database[fileName] = thisSound;
+        // writeln("Loaded sound: ", location);
     }
 
 }
