35a0d57186a6028d1dd5282c1154a0974c97d8ce
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-04-14 22:18:27 -0400

    Nice

--- source/audio/audio.d
+++ source/audio/audio.d
@@ -1,2 +1,27 @@
 module audio.audio;
 
+import raylib;
+
+static final const class Audio {
+static:
+private:
+
+    Sound[string] database;
+
+public: //* BEGIN PUBLIC API.
+
+    void initialize() {
+        InitAudioDevice();
+    }
+
+    void terminate() {
+        foreach (sound; database) {
+            UnloadSound(sound);
+        }
+    }
+
+    void loadSound(string location) {
+
+    }
+
+}
