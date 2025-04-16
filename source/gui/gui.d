0019045e0fe9e81d5bc4d16013b2f69c9024dfd4
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-04-15 22:55:27 -0400

    The formatter kicked in again ahhh

--- source/gui/gui.d
+++ source/gui/gui.d
@@ -328,8 +328,7 @@ public: //* BEGIN PUBLIC API.
                     sizeX,
                     sizeY,
                     buttonColor);
-                const string title = (button.text is null) ? "UNDEFINED"
-                    : button.text;
+                const string title = (button.text is null) ? "UNDEFINED" : button.text;
                 const int adjustment = cast(int) floor((sizeX * 0.5) - (FontHandler.getTextSize(title, 0.25)
                         .x * 0.5));
                 FontHandler.drawShadowed(
