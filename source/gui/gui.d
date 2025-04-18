53eed0def89c575b8069bda74564007c28b100ff
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-04-18 17:24:42 -0400

    And this

--- source/gui/gui.d
+++ source/gui/gui.d
@@ -54,6 +54,7 @@ import std.string;
 *       - TextInputBox  --> Window, Label, TextBox, Button
 */
 
+/// Check if an object is an instance of a class.
 pragma(inline, true)
 T instanceof(T)(Object o) if (is(T == class)) {
     return cast(T) o;
