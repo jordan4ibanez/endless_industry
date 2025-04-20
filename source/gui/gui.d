d6f5179cda8eff4c6b2879ede6e6970ab32139af
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-04-20 01:25:40 -0400

    TextPad logic

--- source/gui/gui.d
+++ source/gui/gui.d
@@ -1106,6 +1106,145 @@ public: //* BEGIN PUBLIC API.
             return false;
         }
 
+        ///? TextPad.
+        bool textPadLogic(ref TextPad textPad) {
+            textPad.mouseHovering = false;
+            const int posX = cast(int) floor(
+                (textPad.position.x * currentGUIScale) + centerX);
+            const int posY = cast(int) floor(
+                ((-textPad.position.y) * currentGUIScale) + centerY);
+            const int sizeX = cast(int) floor(textPad.size.x * currentGUIScale);
+            const int sizeY = cast(int) floor(textPad.size.y * currentGUIScale);
+            const Rectangle buttonRect = Rectangle(
+                posX,
+                posY,
+                sizeX,
+                sizeY);
+            if (CheckCollisionPointRec(mousePos, buttonRect)) {
+                textPad.mouseHovering = true;
+                if (Mouse.isButtonPressed(MouseButton.MOUSE_BUTTON_LEFT)) {
+                    if (focusedTextBox != textPad) {
+                        playButtonSound();
+                    }
+                    {
+                        // Find which character mouse just clicked (if any)
+                        bool usePlaceHolder = (textPad.text is null || textPad.text.length == 0);
+                        if (usePlaceHolder) {
+                            textPad.cursorPosition = 0;
+                        } else {
+                            // This is ultra extremely inefficient.
+                            // But, it works, probably.
+                            double currentWidth = 0;
+                            double width = 0;
+                            int currentHeight = 0;
+                            ulong currentIndexInString = 0;
+                            const string text = (usePlaceHolder) ? textPad.placeholderText
+                                : textPad.text;
+                            if (text !is null && text.length > 0) {
+                                bool foundChar = false;
+                                COLLISION_LOOP_PAD: for (int i = 0; i < text.length;
+                                    i++) {
+                                    const char thisChar = text[i];
+                                    width = FontHandler.getCharWidth(thisChar, 0.25);
+                                    currentWidth += width;
+                                    if (thisChar == '\n') {
+                                        // If newline is reached, it must jump over it.
+                                        currentHeight += cast(int) floor(32 * currentGUIScale);
+                                        currentWidth = 0;
+                                        currentIndexInString = i + 1;
+                                    } else if (currentWidth >= sizeX) {
+                                        currentWidth = width;
+                                        currentHeight += cast(int) floor(32 * currentGUIScale);
+                                        currentIndexInString = i;
+                                    }
+                                    Rectangle charRect = Rectangle(
+                                        cast(int) floor(posX + (currentWidth - width)),
+                                        posY + currentHeight,
+                                        cast(int) floor(width),
+                                        cast(int) floor(32 * currentGUIScale));
+                                    // Hit a character.
+                                    if (CheckCollisionPointRec(mousePos, charRect)) {
+                                        // Break it into two to find out which side to move the cursor into.
+                                        Rectangle charLeft = Rectangle(
+                                            charRect.x,
+                                            charRect.y,
+                                            charRect.width / 2,
+                                            charRect.height
+                                        );
+                                        // If it's not left it's right.
+                                        if (CheckCollisionPointRec(mousePos, charLeft)) {
+                                            textPad.cursorPosition = i;
+                                        } else {
+                                            textPad.cursorPosition = i + 1;
+                                        }
+                                        foundChar = true;
+                                        break COLLISION_LOOP_PAD;
+                                    }
+                                }
+                                // If it hit nothing, just shove it into the last position.
+                                if (!foundChar) {
+                                    textPad.cursorPosition = cast(int) textPad.text.length;
+                                }
+                            }
+                        }
+                    }
+                    focusedTextBox = textPad;
+                }
+            } else {
+                if (Mouse.isButtonPressed(MouseButton.MOUSE_BUTTON_LEFT)) {
+                    if (focusedTextBox == textPad) {
+                        playButtonSound();
+                        keyboardDoingTextInput = false;
+                        focusedTextBox = null;
+                    }
+                }
+            }
+            if (focusedTextBox == textPad) {
+                keyboardDoingTextInput = true;
+                const int input = Keyboard.getCharacterTyped();
+                if (input != 0) {
+                    char[] old = textPad.text.dup;
+                    old.insertInPlace(textPad.cursorPosition, cast(char) input);
+                    textPad.text = old.idup;
+                    cursorMovedUpdate();
+                    textPad.cursorPosition++;
+                } else if (Keyboard.isKeyPressedOrRepeating(KeyboardKey.KEY_BACKSPACE)) {
+                    if (textPad.cursorPosition > 0 && textPad.text.length > 0) {
+                        char[] old = textPad.text.dup;
+                        old = old.remove(textPad.cursorPosition - 1);
+                        textPad.text = old.idup;
+                        textPad.cursorPosition--;
+                        cursorMovedUpdate();
+                    }
+                } else if (Keyboard.isKeyPressedOrRepeating(KeyboardKey.KEY_DELETE)) {
+                    if (textPad.text.length > 0 && textPad.cursorPosition <
+                        textPad.text.length) {
+                        char[] old = textPad.text.dup;
+                        old = old.remove(textPad.cursorPosition);
+                        textPad.text = old.idup;
+                        cursorMovedUpdate();
+                    }
+                } else if (Keyboard.isKeyPressedOrRepeating(KeyboardKey.KEY_ENTER)) {
+                    char[] old = textPad.text.dup;
+                    old.insertInPlace(textPad.cursorPosition, '\n');
+                    textPad.text = old.idup;
+                    textPad.cursorPosition++;
+                    cursorMovedUpdate();
+                } else if (Keyboard.isKeyPressedOrRepeating(KeyboardKey.KEY_RIGHT)) {
+                    if (textPad.cursorPosition < textPad.text.length) {
+                        textPad.cursorPosition++;
+                        cursorMovedUpdate();
+                    }
+                } else if (Keyboard.isKeyPressedOrRepeating(KeyboardKey.KEY_LEFT)) {
+                    if (textPad.cursorPosition > 0) {
+                        textPad.cursorPosition--;
+                        cursorMovedUpdate();
+                    }
+                }
+            }
+            return false;
+        }
+
         //? First pass. 
         //? This blocks things that "expand outwards" from causing
         //? strange overlap collisions with other's that are a statici size.
@@ -1204,146 +1343,14 @@ public: //* BEGIN PUBLIC API.
         //? Second pass.
         //? Statically sized components.
         foreach (thisComponent; currentWindow.componentsInOrder) {
-            //? Button.
             if (Button button = instanceof!Button(thisComponent)) {
                 if (buttonLogic(button)) {
                     break;
                 }
                 //? Text pad.
             } else if (TextPad textPad = instanceof!TextPad(thisComponent)) {
-                textPad.mouseHovering = false;
-                const int posX = cast(int) floor(
-                    (textPad.position.x * currentGUIScale) + centerX);
-                const int posY = cast(int) floor(
-                    ((-textPad.position.y) * currentGUIScale) + centerY);
-                const int sizeX = cast(int) floor(textPad.size.x * currentGUIScale);
-                const int sizeY = cast(int) floor(textPad.size.y * currentGUIScale);
-                const Rectangle buttonRect = Rectangle(
-                    posX,
-                    posY,
-                    sizeX,
-                    sizeY);
-                if (CheckCollisionPointRec(mousePos, buttonRect)) {
-                    textPad.mouseHovering = true;
-                    if (Mouse.isButtonPressed(MouseButton.MOUSE_BUTTON_LEFT)) {
-                        if (focusedTextBox != textPad) {
-                            playButtonSound();
-                        }
-                        {
-                            // Find which character mouse just clicked (if any)
-                            bool usePlaceHolder = (textPad.text is null || textPad.text.length == 0);
-                            if (usePlaceHolder) {
-                                textPad.cursorPosition = 0;
-                            } else {
-                                // This is ultra extremely inefficient.
-                                // But, it works, probably.
-                                double currentWidth = 0;
-                                double width = 0;
-                                int currentHeight = 0;
-                                ulong currentIndexInString = 0;
-                                const string text = (usePlaceHolder) ? textPad.placeholderText
-                                    : textPad.text;
-                                if (text !is null && text.length > 0) {
-                                    bool foundChar = false;
-                                    COLLISION_LOOP_PAD: for (int i = 0; i < text.length;
-                                        i++) {
-                                        const char thisChar = text[i];
-                                        width = FontHandler.getCharWidth(thisChar, 0.25);
-                                        currentWidth += width;
-                                        if (thisChar == '\n') {
-                                            // If newline is reached, it must jump over it.
-                                            currentHeight += cast(int) floor(32 * currentGUIScale);
-                                            currentWidth = 0;
-                                            currentIndexInString = i + 1;
-                                        } else if (currentWidth >= sizeX) {
-                                            currentWidth = width;
-                                            currentHeight += cast(int) floor(32 * currentGUIScale);
-                                            currentIndexInString = i;
-                                        }
-                                        Rectangle charRect = Rectangle(
-                                            cast(int) floor(posX + (currentWidth - width)),
-                                            posY + currentHeight,
-                                            cast(int) floor(width),
-                                            cast(int) floor(32 * currentGUIScale));
-                                        // Hit a character.
-                                        if (CheckCollisionPointRec(mousePos, charRect)) {
-                                            // Break it into two to find out which side to move the cursor into.
-                                            Rectangle charLeft = Rectangle(
-                                                charRect.x,
-                                                charRect.y,
-                                                charRect.width / 2,
-                                                charRect.height
-                                            );
-                                            // If it's not left it's right.
-                                            if (CheckCollisionPointRec(mousePos, charLeft)) {
-                                                textPad.cursorPosition = i;
-                                            } else {
-                                                textPad.cursorPosition = i + 1;
-                                            }
-                                            foundChar = true;
-                                            break COLLISION_LOOP_PAD;
-                                        }
-                                    }
-                                    // If it hit nothing, just shove it into the last position.
-                                    if (!foundChar) {
-                                        textPad.cursorPosition = cast(int) textPad.text.length;
-                                    }
-                                }
-                            }
-                        }
-                        focusedTextBox = textPad;
-                    }
-                } else {
-                    if (Mouse.isButtonPressed(MouseButton.MOUSE_BUTTON_LEFT)) {
-                        if (focusedTextBox == textPad) {
-                            playButtonSound();
-                            keyboardDoingTextInput = false;
-                            focusedTextBox = null;
-                        }
-                    }
-                }
-                if (focusedTextBox == textPad) {
-                    keyboardDoingTextInput = true;
-                    const int input = Keyboard.getCharacterTyped();
-                    if (input != 0) {
-                        char[] old = textPad.text.dup;
-                        old.insertInPlace(textPad.cursorPosition, cast(char) input);
-                        textPad.text = old.idup;
-                        cursorMovedUpdate();
-                        textPad.cursorPosition++;
-                    } else if (Keyboard.isKeyPressedOrRepeating(KeyboardKey.KEY_BACKSPACE)) {
-                        if (textPad.cursorPosition > 0 && textPad.text.length > 0) {
-                            char[] old = textPad.text.dup;
-                            old = old.remove(textPad.cursorPosition - 1);
-                            textPad.text = old.idup;
-                            textPad.cursorPosition--;
-                            cursorMovedUpdate();
-                        }
-                    } else if (Keyboard.isKeyPressedOrRepeating(KeyboardKey.KEY_DELETE)) {
-                        if (textPad.text.length > 0 && textPad.cursorPosition <
-                            textPad.text.length) {
-                            char[] old = textPad.text.dup;
-                            old = old.remove(textPad.cursorPosition);
-                            textPad.text = old.idup;
-                            cursorMovedUpdate();
-                        }
-                    } else if (Keyboard.isKeyPressedOrRepeating(KeyboardKey.KEY_ENTER)) {
-                        char[] old = textPad.text.dup;
-                        old.insertInPlace(textPad.cursorPosition, '\n');
-                        textPad.text = old.idup;
-                        textPad.cursorPosition++;
-                        cursorMovedUpdate();
-                    } else if (Keyboard.isKeyPressedOrRepeating(KeyboardKey.KEY_RIGHT)) {
-                        if (textPad.cursorPosition < textPad.text.length) {
-                            textPad.cursorPosition++;
-                            cursorMovedUpdate();
-                        }
-                    } else if (Keyboard.isKeyPressedOrRepeating(KeyboardKey.KEY_LEFT)) {
-                        if (textPad.cursorPosition > 0) {
-                            textPad.cursorPosition--;
-                            cursorMovedUpdate();
-                        }
-                    }
+                if (textPadLogic(textPad)) {
+                    break;
                 }
                 //? Text box.
             } else if (TextBox textBox = instanceof!TextBox(thisComponent)) {
