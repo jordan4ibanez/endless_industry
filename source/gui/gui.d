09da49b14429e262a4111bae447fbb8bc6cadefa
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-04-24 16:03:11 -0400

    Hmm

--- source/gui/gui.d
+++ source/gui/gui.d
@@ -58,6 +58,8 @@ import utility.instance_of;
 *       - TextInputBox  --> Window, Label, TextBox, Button
 */
 
+package alias ScissorFunction = bool function(const int, const int, const int, const int);
+
 static final const class GUI {
 static:
 package:
