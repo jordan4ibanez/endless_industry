3a8d130ab65e177698ecc53aa415c353218df569
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-04-24 16:19:07 -0400

    Slap this here

--- source/gui/component_draw.d
+++ source/gui/component_draw.d
@@ -109,3 +109,105 @@ void drawButton(ref Component __self, const ref Vec2i center, const StartScissor
         borderColor);
     endScissorComponent();
 }
+
+///? CheckBox.
+void drawCheckBox(ref Component __self, const ref Vec2i center, const StartScissorFunction startScissorComponent,
+    const EndScissorFunction endScissorComponent) {
+    CheckBox checkBox = cast(CheckBox) __self;
+    const int posX = cast(int) floor(
+        (checkBox.position.x * GUI.currentGUIScale) + center.x);
+    const int posY = cast(int) floor(
+        ((-checkBox.position.y) * GUI.currentGUIScale) + center.y);
+    const int sizeX = cast(int) floor(checkBox.size.x * GUI.currentGUIScale);
+    const int sizeY = cast(int) floor(checkBox.size.y * GUI.currentGUIScale);
+    const int statusAreaHeight = cast(int) floor(GUI.currentGUIScale * 32.0);
+    // Initial check to see if this thing should even be drawn.
+    if (startScissorComponent(posX, posY, sizeX, sizeY)) {
+        return;
+    }
+    // First, draw what is basically a regular button.
+    startScissorComponent(posX, posY, sizeX - statusAreaHeight, sizeY);
+    Color buttonColor = (checkBox.mouseHovering) ? checkBox.backgroundColorHover
+        : checkBox.backgroundColor;
+    Color borderColor = (checkBox.mouseHovering) ? checkBox.borderColorHover : checkBox.borderColor;
+    DrawRectangle(
+        posX,
+        posY,
+        sizeX - statusAreaHeight,
+        sizeY,
+        buttonColor);
+    const string title = (checkBox.text is null) ? "UNDEFINED" : checkBox.text;
+    // Attempt to center this into the "mini button".
+    const int adjustment = cast(int) floor(
+        (sizeX * 0.5) - (FontHandler.getTextSize(title, 0.25)
+            .x * 0.5)) - (statusAreaHeight / 2);
+    FontHandler.drawShadowed(
+        title,
+        posX + adjustment,
+        posY,
+        0.25,
+        checkBox.textColor);
+    DrawRectangleLines(
+        posX,
+        posY,
+        sizeX - statusAreaHeight,
+        sizeY,
+        borderColor);
+
+    // Next, draw a toggle indicator.
+    startScissorComponent(posX + sizeX - statusAreaHeight, posY, statusAreaHeight, sizeY);
+
+    // Infill the background.
+    DrawRectangle(
+        posX + sizeX - statusAreaHeight,
+        posY,
+        statusAreaHeight,
+        statusAreaHeight,
+        buttonColor
+    );
+
+    const int halfHeight = statusAreaHeight / 2;
+    const int thirdHeight = statusAreaHeight / 3;
+
+    // Outer circle.
+    DrawCircle(
+        posX + sizeX - halfHeight,
+        posY + halfHeight,
+        thirdHeight,
+        checkBox.backgroundColor
+    );
+    DrawCircleLines(
+        posX + sizeX - halfHeight,
+        posY + halfHeight,
+        thirdHeight,
+        borderColor
+    );
+
+    // Draw the internal circle (if checked).
+    if (checkBox.checked) {
+        const int fifthHeight = statusAreaHeight / 5;
+        DrawCircle(
+            posX + sizeX - halfHeight,
+            posY + halfHeight,
+            fifthHeight,
+            checkBox.checkCircleColor
+        );
+        DrawCircleLines(
+            posX + sizeX - halfHeight,
+            posY + halfHeight,
+            fifthHeight,
+            borderColor
+        );
+    }
+
+    // Then make it look nice with a border.
+    DrawRectangleLines(
+        posX + sizeX - statusAreaHeight,
+        posY,
+        statusAreaHeight,
+        statusAreaHeight,
+        borderColor
+    );
+
+    endScissorComponent();
+}
