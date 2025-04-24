75996e881482d1a990d3bfaf809c2bf0221001fb
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-04-24 16:09:42 -0400

    Base component

--- source/gui/component_draw.d
+++ source/gui/component_draw.d
@@ -7,4 +7,8 @@ import math.vec2i;
 import raylib : Vector2;
 import std.math.rounding;
 
+///? Base component.
+void drawComponent(ref Component __self, const ref Vec2i center, const StartScissorFunction startScissorComponent,
+    const EndScissorFunction endScissorComponent) {
+}
 
