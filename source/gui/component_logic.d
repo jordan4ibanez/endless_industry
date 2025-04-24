06d9779e39c708a35870a6a68d0169be265dc27e
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-04-24 15:12:33 -0400

    Implement base delegate mess

--- source/gui/component_logic.d
+++ source/gui/component_logic.d
@@ -1,2 +1,37 @@
 module gui.component_logic;
 
+import controls.mouse;
+import gui.component;
+import gui.gui;
+import math.vec2i;
+import raylib : CheckCollisionPointRec, Rectangle, Vector2;
+import std.math.rounding;
+
+///? Button.
+bool buttonLogic(ref Component __self, const double currentGUIScale, const ref Vec2i center, const ref Vector2 mousePos) {
+    Button button = cast(Button) __self;
+
+    button.mouseHovering = false;
+    const int posX = cast(int) floor(
+        (button.position.x * currentGUIScale) + center.x);
+    const int posY = cast(int) floor(
+        ((-button.position.y) * currentGUIScale) + center.y);
+    const int sizeX = cast(int) floor(button.size.x * currentGUIScale);
+    const int sizeY = cast(int) floor(button.size.y * currentGUIScale);
+    const Rectangle buttonRect = Rectangle(
+        posX,
+        posY,
+        sizeX,
+        sizeY);
+    // If the mouse is hovering over the button.
+    if (CheckCollisionPointRec(mousePos, buttonRect)) {
+        button.mouseHovering = true;
+        // If the mouse clicks the button.
+        if (Mouse.isButtonPressed(MouseButton.MOUSE_BUTTON_LEFT)) {
+            GUI.playButtonSound();
+            button.clickFunction();
+            return true;
+        }
+    }
+    return false;
+}
