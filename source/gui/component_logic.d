1862d7859a050eecdd086ebcb21adc6fabac42a1
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-04-24 15:43:46 -0400

    Slap it over here

--- source/gui/component_logic.d
+++ source/gui/component_logic.d
@@ -220,3 +220,140 @@ bool textPadLogic(ref Component __self, const ref Vec2i center, const ref Vector
     }
     return false;
 }
+
+///? TextBox.
+bool textBoxLogic(ref Component __self, const ref Vec2i center, const ref Vector2 mousePos,
+    ref bool keyboardDoingTextInput) {
+    TextBox textBox = cast(TextBox) __self;
+    textBox.mouseHovering = false;
+    const int posX = cast(int) floor(
+        (textBox.position.x * GUI.currentGUIScale) + center.x);
+    const int posY = cast(int) floor(
+        ((-textBox.position.y) * GUI.currentGUIScale) + center.y);
+    const int sizeX = cast(int) floor(textBox.size.x * GUI.currentGUIScale);
+    const int sizeY = cast(int) floor(textBox.size.y * GUI.currentGUIScale);
+    const Rectangle buttonRect = Rectangle(
+        posX,
+        posY,
+        sizeX,
+        sizeY);
+    if (CheckCollisionPointRec(mousePos, buttonRect)) {
+        textBox.mouseHovering = true;
+        if (Mouse.isButtonPressed(MouseButton.MOUSE_BUTTON_LEFT)) {
+            if (GUI.focusedComponent != textBox) {
+                GUI.playButtonSound();
+            }
+
+            // Find which character mouse just clicked (if any)
+            bool usePlaceHolder = (textBox.text is null || textBox.text.length == 0);
+            if (usePlaceHolder) {
+                textBox.cursorPosition = 0;
+            } else {
+                // This is ultra extremely inefficient.
+                // But, it works, probably.
+                double currentWidth = 0;
+                double width = 0;
+                int adjustment = 0;
+                const string text = (usePlaceHolder) ? textBox.placeholderText : textBox.text;
+                bool foundChar = false;
+                const double totalSize = FontHandler.getTextSize(textBox.text, 0.25).x;
+
+                if (text is null || text.length <= 0) {
+                    goto SKIP_TEXT_BOX_COLLISION_LOGIC;
+                }
+
+                if (totalSize > sizeX) {
+                    adjustment = cast(int) round(
+                        (totalSize - sizeX) + (3 * GUI.currentGUIScale));
+                }
+
+                COLLISION_LOOP_BOX: for (int i = 0; i < text.length; i++) {
+                    const char thisChar = text[i];
+                    width = FontHandler.getCharWidth(thisChar, 0.25);
+                    currentWidth += width;
+                    Rectangle charRect = Rectangle(
+                        cast(int) floor(
+                            posX + (currentWidth - width)) - adjustment,
+                        posY,
+                        cast(int) floor(width),
+                        cast(int) floor(32 * GUI.currentGUIScale));
+                    // Hit a character.
+                    if (CheckCollisionPointRec(mousePos, charRect)) {
+                        // Break it into two to find out which side to move the cursor into.
+                        Rectangle charLeft = Rectangle(
+                            charRect.x,
+                            charRect.y,
+                            charRect.width / 2,
+                            charRect.height
+                        );
+                        // If it's not left it's right.
+                        if (CheckCollisionPointRec(mousePos, charLeft)) {
+                            textBox.cursorPosition = i;
+                        } else {
+                            textBox.cursorPosition = i + 1;
+                        }
+                        foundChar = true;
+                        break COLLISION_LOOP_BOX;
+                    }
+                }
+                // If it hit nothing, just shove it into the last position.
+                if (!foundChar) {
+                    textBox.cursorPosition = cast(int) textBox.text.length;
+                }
+
+            SKIP_TEXT_BOX_COLLISION_LOGIC:
+
+            }
+
+            GUI.focusedComponent = textBox;
+        }
+    } else {
+        if (Mouse.isButtonPressed(MouseButton.MOUSE_BUTTON_LEFT)) {
+            if (GUI.focusedComponent == textBox) {
+                GUI.playButtonSound();
+                keyboardDoingTextInput = false;
+                GUI.focusedComponent = null;
+            }
+        }
+    }
+    if (GUI.focusedComponent == textBox) {
+        keyboardDoingTextInput = true;
+        const int input = Keyboard.getCharacterTyped();
+        if (input != 0) {
+            if (textBox.text.length < textBox.maxCharacters) {
+                char[] old = textBox.text.dup;
+                old.insertInPlace(textBox.cursorPosition, cast(char) input);
+                textBox.text = old.idup;
+                GUI.cursorMovedUpdate();
+                textBox.cursorPosition++;
+            }
+        } else if (Keyboard.isKeyPressedOrRepeating(KeyboardKey.KEY_BACKSPACE)) {
+            if (textBox.cursorPosition > 0 && textBox.text.length > 0) {
+                char[] old = textBox.text.dup;
+                old = old.remove(textBox.cursorPosition - 1);
+                textBox.text = old.idup;
+                textBox.cursorPosition--;
+                GUI.cursorMovedUpdate();
+            }
+        } else if (Keyboard.isKeyPressedOrRepeating(KeyboardKey.KEY_DELETE)) {
+            if (textBox.text.length > 0 && textBox.cursorPosition <
+                textBox.text.length) {
+                char[] old = textBox.text.dup;
+                old = old.remove(textBox.cursorPosition);
+                textBox.text = old.idup;
+                GUI.cursorMovedUpdate();
+            }
+        } else if (Keyboard.isKeyPressedOrRepeating(KeyboardKey.KEY_RIGHT)) {
+            if (textBox.cursorPosition < textBox.text.length) {
+                textBox.cursorPosition++;
+                GUI.cursorMovedUpdate();
+            }
+        } else if (Keyboard.isKeyPressedOrRepeating(KeyboardKey.KEY_LEFT)) {
+            if (textBox.cursorPosition > 0) {
+                textBox.cursorPosition--;
+                GUI.cursorMovedUpdate();
+            }
+        }
+    }
+    return false;
+}
