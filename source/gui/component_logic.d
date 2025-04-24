e04fbce5a7946e65f98ca3fc492bbd04783d48af
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-04-24 15:21:32 -0400

    Bolt it into a mess

--- source/gui/component_logic.d
+++ source/gui/component_logic.d
@@ -43,3 +43,33 @@ bool buttonLogic(ref Component __self, const double currentGUIScale, const ref V
     }
     return false;
 }
+
+///? CheckBox.
+bool checkBoxLogic(ref Component __self, const double currentGUIScale, const ref Vec2i center,
+    const ref Vector2 mousePos) {
+    CheckBox checkBox = cast(CheckBox) __self;
+    checkBox.mouseHovering = false;
+    const int posX = cast(int) floor(
+        (checkBox.position.x * currentGUIScale) + center.x);
+    const int posY = cast(int) floor(
+        ((-checkBox.position.y) * currentGUIScale) + center.y);
+    const int sizeX = cast(int) floor(checkBox.size.x * currentGUIScale);
+    const int sizeY = cast(int) floor(checkBox.size.y * currentGUIScale);
+    const Rectangle buttonRect = Rectangle(
+        posX,
+        posY,
+        sizeX,
+        sizeY);
+    // If the mouse is hovering over the button.
+    if (CheckCollisionPointRec(mousePos, buttonRect)) {
+        checkBox.mouseHovering = true;
+        // If the mouse clicks the button.
+        if (Mouse.isButtonPressed(MouseButton.MOUSE_BUTTON_LEFT)) {
+            GUI.playButtonSound();
+            checkBox.checked = !checkBox.checked;
+            checkBox.clickFunction();
+            return true;
+        }
+    }
+    return false;
+}
