3202d5bf6bcc68a0d24c8f47f1891154171bddff
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-04-24 04:47:53 -0400

    Update inventory.d

--- source/game/inventory.d
+++ source/game/inventory.d
@@ -3,12 +3,12 @@ module game.inventory;
 final class Inventory {
 private:
 
-    int __size = 8;
+    int __size = 10;
+    int __width = 10;
+
 public:
 
-    this() {
-    }
-
+    /// Set the size of the inventory.
     @property void size(int size) {
         this.__size = size;
     }
