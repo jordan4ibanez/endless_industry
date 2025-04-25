c84f9552e581925245ae2b756c7bcee243731f4f
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-04-25 10:26:15 -0400

    Update item.d

--- source/game/item.d
+++ source/game/item.d
@@ -1,5 +1,14 @@
 module game.item;
 
+struct ItemDefinition {
+    string name = null;
+    string texture = null;
+
+    //! DO NOT USE.
+    int id = -1;
+    ulong texturePointsIndex = 0;
+}
+
 struct Item {
     int id = 0;
     int count = 0;
@@ -9,6 +18,12 @@ static final const class ItemHandler {
 static:
 private:
 
+    Item[string] nameDatabase;
+
 public:
+
+    void registerItem(const string name, Item item) {
+
+    }
 
 }
